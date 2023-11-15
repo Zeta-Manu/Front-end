9af570af6038fc9797ac7a3e2dccecba9aa46dbd
tsebtseb <tsebtseb.alone@gmail.com>
2023-11-15 20:31:50 +0700

    testtest

--- src/Recognition.jsx
+++ src/Recognition.jsx
@@ -10,6 +10,7 @@ const Recognition = () => {
   const confident = 100; //from backend
 
 
+
   /*function to on/off stream*/
   const handleStream = async () => {
     if (!streamActive) { /*stream currently not active=>turn on*/
@@ -44,20 +45,97 @@ const Recognition = () => {
       }
     };
 
+    // recorder.onstop = () => {
+    //   const blob = new Blob(chunks, { type: 'video/mp4' });
+    //   const videoUrl = URL.createObjectURL(blob);
+    //   console.log('Recording Finished. Video URL:', videoUrl);
+
+    //   //temporary download link
+    //   const a = document.createElement('a');
+    //   a.href = videoUrl;
+    //   a.download = 'recorded-video.mp4';
+    //   a.style.display = 'none';
+    //   document.body.appendChild(a);
+    //   a.click();
+    //   document.body.removeChild(a);
+    //   uploadVideo(videoUrl);
+    // };
+
     recorder.onstop = () => {
       const blob = new Blob(chunks, { type: 'video/mp4' });
-      const videoUrl = URL.createObjectURL(blob);
-      console.log('Recording Finished. Video URL:', videoUrl);
 
-      //temporary download link
-      const a = document.createElement('a');
-      a.href = videoUrl;
-      a.download = 'recorded-video.mp4';
-      a.style.display = 'none';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
+      console.log('Recording Finished',blob);
+
+      uploadVideo(blob);
     };
+
+    const uploadVideo = async (videoBlob) => {
+      try {
+        const formData = new FormData();
+
+        formData.append('video', videoBlob, 'recorded-video.mp4');
+
+        const response = await fetch('http://localhost:8080/api/postVideo', {
+          method: 'POST',
+          mode: 'cors',
+          body: formData,
+        });
+
+        const errorCode = response.status;
+
+        switch (errorCode) {
+          case 200:
+            console.log("success")
+            break;
+          case 400:
+            console.error("...")
+            break;
+          case 500:
+            console.error("Internal server error")
+            break;
+          default:
+            console.error("Unknown error")
+            break;
+        }
+      } catch (error) {
+        console.error('Error uploading video:', error);
+      }
+    };
+
+    // const uploadVideo = async (videoUrl) => {
+  //   try {
+  //     const response = await fetch('http://localhost:8080/api/postVideo', {
+  //       method: 'POST',
+  //       mode: "cors",
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         // You might need additional headers depending on your backend requirements
+  //       },
+  //       body: JSON.stringify({ videoUrl }), // Send the video URL in the request body
+  //     });
+
+  //     if (response.ok) {
+  //       const result = await response.json();
+
+  //       // Check the status code from the backend
+  //       if (result.statusCode === 200) {
+  //         console.log('File uploaded successfully!');
+  //       } else {
+  //         console.error('Failed to upload video. Status:', result.statusCode);
+  //         // Handle failure
+  //       }
+  //     } else if (response.status === 400) {
+  //       console.error('Bad Request. Please check your request payload.');
+  //       // Handle Bad Request
+  //     } else {
+  //       console.error('Internal Server Error. Please try again later.');
+  //       // Handle Internal Server Error
+  //     }
+  //   } catch (error) {
+  //     console.error('Error uploading video:', error);
+  //   }
+  // };
+
 
     recorder.start();
     setMediaRecorder(recorder);
@@ -69,20 +147,20 @@ const Recognition = () => {
     }
   };
 
-  const getColor=(confident) => {
+  const getColor = (confident) => {
     let opacity;
-    if(confident<50){
-      opacity=(confident-1)/49;
-      opacity=1-opacity;
-      opacity=Math.max(opacity, 0.3);
+    if (confident < 50) {
+      opacity = (confident - 1) / 49;
+      opacity = 1 - opacity;
+      opacity = Math.max(opacity, 0.3);
       return `rgba(255, 0, 0, ${opacity})`; // Red=>inverse opacity
-    } else if (confident>=50 && confident<=60) {
-      opacity=(confident-50)/10;
-      opacity=Math.max(opacity, 0.5);
+    } else if (confident >= 50 && confident <= 60) {
+      opacity = (confident - 50) / 10;
+      opacity = Math.max(opacity, 0.5);
       return `rgba(255, 204, 0, ${opacity})`; // Yellow
     } else {//61=>100
-      opacity=(confident-60)/40;
-      opacity=Math.max(opacity, 0.5);
+      opacity = (confident - 60) / 40;
+      opacity = Math.max(opacity, 0.5);
       return `rgba(0, 255, 0, ${opacity})`; // Green
     }
   }
@@ -96,36 +174,36 @@ const Recognition = () => {
           <h1 className="text-black font-italic">Translation...</h1>
         </div>
         <div className="flex rounded-lg h-2/6 mx-10 mt-4 bg-[#e7f2f6] p-2"> {/*#222222, #F7F7F7*/}
-        <CircularProgressbar
-          value={confident}
-          text={`${confident}%`}
-          styles={{
-            root: {},
-            path: {
-              stroke: getColor(confident),
-              strokeLinecap: 'butt',
-              transition: 'stroke-dashoffset 0.5s east 0s',
-              transform: 'rotate(0.25turn)',
-              transformOrigin: 'center center',
-            },
-            trail: {
-              stroke: '#d6d6d6',
-              strokeLinecap: 'butt',
-              transform: 'rotate(0.25turn)',
-              transformOrigin: 'center center',
-            },
-            text: {
-              fill: getColor(confident), // Change text color dynamically
-              fontSize: '25px',
-              dominantBaseline: 'middle',
-              fontWeight: 'bold',
-              textAnchor: 'middle',
-            },
-            background: {
-              fill: '#3e98c7',
-            },
-          }}
-        />
+          <CircularProgressbar
+            value={confident}
+            text={`${confident}%`}
+            styles={{
+              root: {},
+              path: {
+                stroke: getColor(confident),
+                strokeLinecap: 'butt',
+                transition: 'stroke-dashoffset 0.5s east 0s',
+                transform: 'rotate(0.25turn)',
+                transformOrigin: 'center center',
+              },
+              trail: {
+                stroke: '#d6d6d6',
+                strokeLinecap: 'butt',
+                transform: 'rotate(0.25turn)',
+                transformOrigin: 'center center',
+              },
+              text: {
+                fill: getColor(confident), // Change text color dynamically
+                fontSize: '25px',
+                dominantBaseline: 'middle',
+                fontWeight: 'bold',
+                textAnchor: 'middle',
+              },
+              background: {
+                fill: '#3e98c7',
+              },
+            }}
+          />
         </div>
       </div>
       <div className="flex flex-col w-2/3 mt-20">
